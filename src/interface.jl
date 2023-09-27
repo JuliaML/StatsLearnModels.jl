44d167a1f86e5b2f6b5ce2bf523b2e97d908c028
Elias Carvalho <eliascarvdev@gmail.com>
2023-09-27 15:54:11 -0300

    Add 'Learn' transform

---
+++ src/interface.jl
@@ -0,0 +1,27 @@
+"""
+    StatsLearnModels.fit(model, input, output) -> FittedModel
+
+Fit statistical learning `model` using features in `input` table
+and targets in `output` table. Returns a fitted model with all
+the necessary information for prediction with the `predict` function.
+"""
+function fit end
+
+"""
+    StatsLearnModels.predict(model::FittedModel, table)
+
+Predict the target values using the fitted statistical learning `model`
+and a new `table` of features.  
+"""
+function predict end
+
+"""
+    StatsLearnModels.FittedModel(model, cache)
+
+Wrapper type used to save learning model and auxiliary
+variables needed for prediction.
+"""
+struct FittedModel{M,C}
+  model::M
+  cache::C
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,39 @@
-import StatsLearnModels as SLM
+using StatsLearnModels
 using MLJ, MLJDecisionTreeInterface
 using DataFrames
 using Random
 using Test
 
+const SLM = StatsLearnModels
+
 @testset "StatsLearnModels.jl" begin
-  Random.seed!(123)
-  iris = DataFrame(load_iris())
-  input = iris[:, Not(:target)]
-  output = iris[:, [:target]]
-  Tree = @load(DecisionTreeClassifier, pkg=DecisionTree, verbosity=0)
-  train, test = partition(1:nrow(input), 0.7, rng=123)
-  fmodel = SLM.fit(Tree(), input[train, :], output[train, :])
-  pred = SLM.predict(fmodel, input[test, :])
-  accuracy = count(pred.target .== output.target[test]) / length(test)
-  @test accuracy > 0.9
+  @testset "interface" begin
+    Random.seed!(123)
+    iris = DataFrame(load_iris())
+    input = iris[:, Not(:target)]
+    output = iris[:, [:target]]
+    Tree = @load(DecisionTreeClassifier, pkg=DecisionTree, verbosity=0)
+    train, test = partition(1:nrow(input), 0.7, rng=123)
+    fmodel = SLM.fit(Tree(), input[train, :], output[train, :])
+    pred = SLM.predict(fmodel, input[test, :])
+    accuracy = count(pred.target .== output.target[test]) / length(test)
+    @test accuracy > 0.9
+  end
+
+  @testset "Learn" begin
+    Random.seed!(123)
+    iris = DataFrame(load_iris())
+    target = :target
+    incols = setdiff(propertynames(iris), [target])
+    Tree = @load(DecisionTreeClassifier, pkg=DecisionTree, verbosity=0)
+    train, test = partition(1:nrow(iris), 0.7, rng=123)
+    transform = Learn(Tree(), iris[train, :], incols, target)
+    pred = transform(iris[test, :])
+    accuracy = count(pred.target .== iris.target[test]) / length(test)
+    @test accuracy > 0.9
+
+    # throws
+    # test data is not a table
+    @test_throws ArgumentError Learn(Tree(), nothing, incols, outcols)
+  end
 end
