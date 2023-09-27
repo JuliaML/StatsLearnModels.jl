5c56538caedd0b36989db880703dec675c6b39a5
Elias Carvalho <eliascarvdev@gmail.com>
2023-09-27 14:17:18 -0300

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
-using StatsLearnModels
+import StatsLearnModels as SLM
+using MLJ, MLJDecisionTreeInterface
+using DataFrames
 using Test
 
 @testset "StatsLearnModels.jl" begin
-    # Write your tests here.
+  iris = DataFrame(load_iris())
+  input = iris[:, Not(:target)]
+  output = iris[:, [:target]]
+  Tree = @load(DecisionTreeClassifier, pkg=DecisionTree, verbosity=0)
+  train, test = partition(1:nrow(input), 0.7, rng=123)
+  fmodel = SLM.fit(Tree(), input[train, :], output[train, :])
+  pred = SLM.predict(fmodel, input[test, :])
+  accuracy = count(pred.target .== output.target[test]) / length(test)
+  @test accuracy > 0.9
 end
