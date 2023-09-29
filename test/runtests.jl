e0d55a82c3560836a969f6f47f2d61ceeb52165f
Elias Carvalho <eliascarvdev@gmail.com>
2023-09-29 08:15:20 -0300

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using StatsLearnModels
 using MLJ, MLJDecisionTreeInterface
+using TableTransforms
 using DataFrames
 using Random
 using Test
@@ -23,17 +24,18 @@ const SLM = StatsLearnModels
   @testset "Learn" begin
     Random.seed!(123)
     iris = DataFrame(load_iris())
-    target = :target
-    incols = setdiff(propertynames(iris), [target])
+    outcol = :target
+    incols = setdiff(propertynames(iris), [outcol])
     Tree = @load(DecisionTreeClassifier, pkg=DecisionTree, verbosity=0)
     train, test = partition(1:nrow(iris), 0.7, rng=123)
-    transform = Learn(Tree(), iris[train, :], incols, target)
+    transform = Learn(Tree(), iris[train, :], incols, outcol)
+    @test !isrevertible(transform)
     pred = transform(iris[test, :])
     accuracy = count(pred.target .== iris.target[test]) / length(test)
     @test accuracy > 0.9
 
     # throws
     # training data is not a table
-    @test_throws ArgumentError Learn(Tree(), nothing, incols, outcols)
+    @test_throws ArgumentError Learn(Tree(), nothing, incols, outcol)
   end
 end
