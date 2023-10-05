1112c1c7f89949e5db10f3fb6825e2ddd900dc74
Elias Carvalho <eliascarvdev@gmail.com>
2023-10-05 11:08:11 -0300

    Add GLM.jl models

--- src/StatsLearnModels.jl
+++ src/StatsLearnModels.jl
@@ -9,21 +9,28 @@ using ColumnSelectors: selector
 using TableTransforms: StatelessFeatureTransform
 import TableTransforms: applyfeat, isrevertible
 
+import GLM
 import DecisionTree as DT
 using DecisionTree: AdaBoostStumpClassifier, DecisionTreeClassifier, RandomForestClassifier
 using DecisionTree: DecisionTreeRegressor, RandomForestRegressor
+using Distributions: UnivariateDistribution
 
 include("interface.jl")
 include("models/decisiontree.jl")
+include("models/glm.jl")
 include("learn.jl")
 
 export
   # models
+  # DecisionTree
   AdaBoostStumpClassifier,
   DecisionTreeClassifier,
   RandomForestClassifier,
   DecisionTreeRegressor,
   RandomForestRegressor,
+  # GLM
+  LinearRegressor,
+  GeneralizedLinearRegressor,
 
   # transform
   Learn
